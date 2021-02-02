3492a327c6d47f9190e8607208273eaed3936a11
Alex <koihoz@mail.ru>
2021-02-02 20:43:41 +0300

    добавили новые функции

--- js/main.js
+++ js/main.js
@@ -1,16 +1,16 @@
 //Функция, возвращающая случайное целое число из переданного диапазона включительно.
-function getRandomIntInclusive (min, max) {
-  min = Math.ceil(min);
-  max = Math.floor(max);
-  return Math.floor(Math.random() * (max - min + 1)) + min; //Максимум и минимум включаются
+const getRandomIntInclusive = (min, max) => {
+  const minValue = Math.ceil(min);
+  const maxValue = Math.floor(max);
+  return Math.floor(Math.random() * (maxValue - minValue + 1)) + minValue; //Максимум и минимум включаются
 }
 
 getRandomIntInclusive (2, 10);
 
 //Функция, возвращающая случайное число с плавающей точкой из переданного диапазона включительно.
-function getRandomNumber (from, to, decimal = 2) {
+const getRandomNumber = (from, to, decimal = 2) => {
   if (to > from && from >= 0) {
-    return +((Math.random() * (to - from) + from).toFixed(decimal));
+    return Number((Math.random() * (to - from) + from).toFixed(decimal));
   }
 
   throw new Error('Input data error');
